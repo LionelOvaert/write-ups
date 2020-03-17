bb625995c5180a088abfb4415c4d1a2e9bb098a4
Lionel Ovaert <lionel.ovaert@live.be>
2020-03-17 05:00:46 +0100

    Scrambled: added comments

--- b01lers_ctf_2020/scrambled/cut_transmissions.py
+++ b01lers_ctf_2020/scrambled/cut_transmissions.py
@@ -4,7 +4,9 @@ with open("transmissions") as f:
     data = f.readlines()
     parts = {}
     for line in data:
+        # URL decode the line and remove the garbage
         line = urllib.parse.unquote(line.strip().replace("kxkxkxkxsh", ""))
+        # 2-digit number
         if line[-2:].isnumeric():
             nb = int(line[-2:])
             part = line[: len(line) - 3]
@@ -13,6 +15,7 @@ with open("transmissions") as f:
                 # Last part contains 2 elements
                 part = line[len(part):len(line) - 2]
                 parts[nb + 1] = part
+        # 1-digit number
         else:
             nb = int(line[-1:])
             part = line[: len(line) - 2]
